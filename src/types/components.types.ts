d13a2935366d909310b807107d63d12a2b0f46f5
Marlon Vélez <150103365+MarlonVelezB@users.noreply.github.com>
2024-11-17 23:42:27 -0500

    Se agrego logica para agregar info a una tabla.

---
+++ src/types/components.types.ts
@@ -0,0 +1,7 @@
+import { TableProps } from "antd/es/table";
+
+export interface TableComponentProps {
+    dataType?: any;
+    columns?: TableProps<any>["columns"];
+    dataSource: any;
+}
---
+++ src/types/working-info.models.ts
@@ -0,0 +1,9 @@
+export interface WorkingTimeInfo{
+    _id?: string; 
+    key?: any;
+    jobStartTime: string;
+    lunchStartTime: string;
+    lunchEndTime: string;
+    jobEndTime: string;
+    isLunchBreakRequired: boolean;
+}
